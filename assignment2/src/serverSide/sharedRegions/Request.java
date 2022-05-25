bf8c9c477c61dd978cdb03f3aab8f540c4dffa25
Diogo Vicente <diogo.vicente@hotmail.com>
2022-05-25 23:02:10 +0100

    request

---
+++ assignment2/src/serverSide/sharedRegions/Request.java
@@ -0,0 +1,12 @@
+package serverSide.sharedRegions;
+
+public class Request {
+	
+	public int id;
+	public char type; 
+	
+	public Request(int id, char type) {
+		this.id = id;
+		this.type = type;
+	}
+}
