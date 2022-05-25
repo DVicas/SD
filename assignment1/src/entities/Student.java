e6e55b1c598e6ff533a023bc2911300de33a7ae9
Diogo Vicente <diogo.vicente@hotmail.com>
2022-05-25 14:17:18 +0100

    Ass2 start

---
+++ assignment1/src/entities/Student.java
@@ -0,0 +1,78 @@
+package entities;
+
+import sharedRegions.Bar;
+
+import sharedRegions.Table;
+
+public class Student extends Thread {
+	
+	private int studentId;
+	private int studentState;
+	
+	private Table table;
+	private Bar bar;
+	
+	public Student(String name, int studentId, Table table, Bar bar) {
+		super(name);
+		this.studentId = studentId;
+		this.table = table;
+		this.bar = bar;
+		studentState = StudentStates.GOING_TO_THE_RESTAURANT;
+	}
+	
+	public int getStudentId() { return studentId; }
+	
+	public int getStudentState() { return studentState; }
+	
+	public void setStudentState(int studentState) { this.studentState = studentState; } 
+	
+	public void walkABit() {
+		try
+	      { sleep ((long) (1 + 40 * Math.random ()));
+	      }
+	      catch (InterruptedException e) {}
+	}
+	
+	@Override
+	public void run() {
+		
+		walkABit();
+		bar.enter();
+		table.seatAtTable();
+		table.readTheMenu();
+		
+		if (table.getFirstStudent() == studentId) {
+			table.prepareOrder();							
+			
+			while(!table.hasEverybodyChosen()) {
+				table.addUpOnesChoice();
+			}
+			
+			bar.callWaiter();
+			table.describeOrder();
+			table.joinTalk();
+		} else {
+			table.informCompanion();
+		}
+		
+		while(!table.everyoneHasEaten()) {
+			
+			table.startEating();
+			table.endEating();
+			
+			while(!table.hasEveryoneFinishedPortion());
+			
+			if(table.lastToEat() == studentId) {
+				bar.signalWaiter();
+			}
+		}
+		
+		if(table.shouldHaveArrivedEarlier()) {
+			bar.signalWaiter();
+			table.honorTheBill();
+		}
+		
+		bar.exit();
+	}
+	
+}
